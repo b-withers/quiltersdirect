b31b29c0f9423e318746d07fad71b276c6b67883
Brady Withers <brady.withers@outlook.com>
2019-05-06 20:43:38 -0600

    comment cleanup

--- assets/javascript/customscript.js
+++ assets/javascript/customscript.js
@@ -10,7 +10,7 @@ function passWord() {
         }
         testV += 1;
         var pass1 =
-            prompt('Access Denied - Password Incorrect, Please Try Again.', 'Password');
+            prompt('YOU SHALL NOT PASS', 'Password');
     }
     if (pass1.toLowerCase() != "password" & testV == 3)
         history.go(-1);
