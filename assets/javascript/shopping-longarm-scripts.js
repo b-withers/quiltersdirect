d27221651addb3a09446dbd53be63466c32a444a
gleekzorp <gleek.dan@gmail.com>
2019-07-30 12:51:21 -0600

    longarm shopping experience jquery complete.  Needs some style adjustments, spell checking and product needs descriptions

--- assets/javascript/shopping-longarm-scripts.js
+++ assets/javascript/shopping-longarm-scripts.js
@@ -20,53 +20,203 @@ $(document).ready(function() {
     $(`.circle-${x}`).css("background-color", "var(--purpleTwo)");
   }
 
-  // Sitdown
-  $('#machine-one-button').click(function() {
-    $('.cart-item-names').append('DYNA SITDOWN ');
+  function hideMachines() {
     $('#machine-one').hide();
     $('#machine-two').hide();
     $('#machine-three').hide();
-    $('#frame-one').css('display', 'flex');
+  }
+
+  function hideFrames(machine) {
+    if (machine === 'sitdown') {
+      $('#frame-one').hide();
+    } else {
+      $('#frame-two').hide();
+      $('#frame-three').hide();
+      $('#frame-four').hide();
+    }
+  }
+
+  function showFrames(machine) {
+    if (machine === 'sitdown') {
+      $('#frame-one').css('display', 'flex');
+    } else {
+      $('#frame-two').css('display', 'flex');
+      $('#frame-three').css('display', 'flex');
+      $('#frame-four').css('display', 'flex');
+    }
+  }
+
+  function showAutomation(machine) {
+    if (machine === 'sitdown') {
+      $('#automation-not-available').css('display', 'flex');
+    } else {
+      $('#automation-one').css('display', 'flex');
+      $('#automation-two').css('display', 'flex');
+      $('#automation-skip').css('display', 'flex');
+    }
+  }
+
+  function hideAutomation(machine) {
+    if (machine === 'sitdown') {
+      $('#automation-not-available').hide();
+    } else {
+      $('#automation-one').hide();
+      $('#automation-two').hide();
+      $('#automation-skip').hide();
+    }
+  }
+
+  function showAccessories(machine) {
+    if (machine === 'sitdown') {
+      $('#accessories-one').css('display', 'flex');
+      $('#accessories-two').css('display', 'flex');
+    } else {
+      $('#accessories-three').css('display', 'flex');
+      $('#accessories-four').css('display', 'flex');
+      $('#accessories-five').css('display', 'flex');
+      $('#accessories-skip').css('display', 'flex');
+    }
+  }
+
+  function hideAccessories(machine) {
+    if (machine === 'sitdown') {
+      $('#accessories-one').hide();
+      $('#accessories-two').hide();
+    } else {
+      $('#accessories-three').hide();
+      $('#accessories-four').hide();
+      $('#accessories-five').hide();
+      $('#accessories-skip').hide();
+    }
+  }
+
+  function showWarranty() {
+    $('#warranty-one').css('display', 'flex');
+    $('#warranty-two').css('display', 'flex');
+  }
+
+  function hideWarranty() {
+    $('#warranty-one').hide();
+    $('#warranty-two').hide();
+  }
+
+  // Sitdown
+  $('#machine-one-button').click(function() {
+    $('.cart-item-names').append('DYNA SITDOWN ');
+    hideMachines()
+    showFrames('sitdown')
     changeCircle('one', 'two')
   })
   $('#frame-one-button').click(function() {
     $('.cart-item-names').append('+ DYNA SITDOWN TABLE ');
-    $('#frame-one').hide();
-    $('#automation-not-available').css('display', 'flex');
+    hideFrames('sitdown')
+    showAutomation('sitdown')
     changeCircle('two', 'three')
   })
   $('#automation-not-available-button').click(function() {
-    $('#automation-not-available').hide();
-    $('#accessories-one').css('display', 'flex');
-    $('#accessories-two').css('display', 'flex');
+    hideAutomation('sitdown')
+    showAccessories('sitdown')
     changeCircle('three', 'four')
   })
   $('#accessories-one-button').click(function() {
     $('.cart-item-names').append('+ DYNA SITDOWN ESSENTIAL PACKAGE ');
-    $('#accessories-one').hide();
-    $('#accessories-two').hide();
-    $('#warranty-one').css('display', 'flex');
-    $('#warranty-two').css('display', 'flex');
+    hideAccessories('sitdown')
+    showWarranty()
     changeCircle('four', 'five')
   })
   $('#accessories-two-button').click(function() {
     $('.cart-item-names').append('+ DYNA SITDOWN CREATIVE PACKAGE ');
-    $('#accessories-two').hide();
-    $('#accessories-one').hide();
-    $('#warranty-one').css('display', 'flex');
-    $('#warranty-two').css('display', 'flex');
+    hideAccessories('sitdown')
+    showWarranty()
     changeCircle('four', 'five')
   })
+  // Warranty GOTO:  Common All Machines
+
+
+  // Dynasty A4
+  $('#machine-two-button').click(function() {
+    $('.cart-item-names').append('DYNASTY A4 ');
+    hideMachines();
+    showFrames();
+    changeCircle('one', 'two')
+  })
+  // Dynasty A4 With Adjustable Handlebar and Light Package
+  $('#machine-three-button').click(function() {
+    $('.cart-item-names').append('DYNASTY A4 WITH ADJ HANDLEBAR & LIGHT PKG ');
+    hideMachines();
+    showFrames();
+    changeCircle('one', 'two')
+  })
+  // Frames GOTO:  Common Dynasty A4 & Dynasty A4 With Adjustable Handlebar and Light Package
+  // Automation GOTO:  Common Dynasty A4 & Dynasty A4 With Adjustable Handlebar and Light Package
+  // Accessories GOTO:  Common Dynasty A4 & Dynasty A4 With Adjustable Handlebar and Light Package
+  // Warranty GOTO:  Common All Machines
+
+
+  // Common All Machines
   $('#warranty-one-button').click(function() {
-    $('.cart-item-names').append('+ FREE MONEY BACK GUARANTEE & WARRANTY ');
-    $('#warranty-one').hide();
-    $('#warranty-two').hide();
+    $('.cart-item-names').append('+ FREE MONEY BACK GUARANTEE & WARRANTY');
+    hideWarranty()
     changeLastCircle('five')
   })
   $('#warranty-two-button').click(function() {
-    $('.cart-item-names').append('+ WARRANTY UPGRADE ');
-    $('#warranty-two').hide();
-    $('#warranty-one').hide();
+    $('.cart-item-names').append('+ WARRANTY UPGRADE');
+    hideWarranty()
     changeLastCircle('five')
+  })
+
+  // Common Dynasty A4 & Dynasty A4 With Adjustable Handlebar and Light Package
+  $('#frame-two-button').click(function() {
+    $('.cart-item-names').append("+ GRACE Q-ZONE HOOP QUILTING FRAME ");
+    hideFrames()
+    showAutomation()
+    changeCircle('two', 'three')
+  })
+  $('#frame-three-button').click(function() {
+    $('.cart-item-names').append("+ PHOENIX FRAME 10' ");
+    hideFrames()
+    showAutomation()
+    changeCircle('two', 'three')
+  })
+  $('#frame-four-button').click(function() {
+    $('.cart-item-names').append("+ PHOENIX FRAME 12' ");
+    hideFrames()
+    showAutomation()
+    changeCircle('two', 'three')
+  })
+  $('#automation-one-button').click(function() {
+    $('.cart-item-names').append("+ QCT5 BEGINNINGS ");
+    hideAutomation()
+    showAccessories()
+    changeCircle('three', 'four')
+  })
+  $('#automation-two-button').click(function() {
+    $('.cart-item-names').append("+ QCT5 PRO ");
+    hideAutomation()
+    showAccessories()
+    changeCircle('three', 'four')
+  })
+  $('#automation-skip-button').click(function() {
+    hideAutomation()
+    showAccessories()
+    changeCircle('three', 'four')
+  })
+  $('#accessories-three-button').click(function() {
+    $('.cart-item-names').append('+ DYNASTY ESSENTIAL PACKAGE ');
+    hideAccessories()
+    showWarranty()
+    changeCircle('four', 'five')
+  })
+  $('#accessories-four-button').click(function() {
+    $('.cart-item-names').append('+ DYNASTY CREATIVE PACKAGE ');
+    hideAccessories()
+    showWarranty()
+    changeCircle('four', 'five')
+  })
+  $('#accessories-five-button').click(function() {
+    $('.cart-item-names').append('+ DYNASTY ULTIMATE PACKAGE ');
+    hideAccessories()
+    showWarranty()
+    changeCircle('four', 'five')
   })
 })
